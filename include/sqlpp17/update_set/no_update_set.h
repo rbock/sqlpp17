d4c19057b2898cbcbb3f22d439e2b6f0a1e7969c
rbock <rbock@eudoxos.de>
2017-02-27 17:32:35 +0100

    Added an update skeleton

---
+++ include/sqlpp17/update_set/no_update_set.h
@@ -0,0 +1,108 @@
+#pragma once
+
+/*
+Copyright (c) 2016, Roland Bock
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this
+   list of conditions and the following disclaimer in the documentation and/or
+   other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <sqlpp17/statement.h>
+#include <sqlpp17/update_set/update_set.h>
+
+namespace sqlpp
+{
+  SQLPP_WRAPPED_STATIC_ASSERT(assert_update_set_arg_is_not_conditionless_join,
+                              "update_set() arg must not be a conditionless join, use .on() or .unconditionally()");
+  SQLPP_WRAPPED_STATIC_ASSERT(assert_update_set_arg_is_table, "update_set() arg has to be a table or join");
+  SQLPP_WRAPPED_STATIC_ASSERT(assert_update_set_arg_no_required_tables,
+                              "update_set() arg must not depend on other tables");
+
+  template <typename T>
+  constexpr auto check_update_set_arg(const T&)
+  {
+    if
+      constexpr(is_conditionless_join_v<T>)
+      {
+        return failed<assert_update_set_arg_is_not_conditionless_join>{};
+      }
+    else if
+      constexpr(!is_table_v<T>)
+      {
+        return failed<assert_update_set_arg_is_table>{};
+      }
+    else if
+      constexpr(!required_tables_of_v<T>.empty())
+      {
+        return failed<assert_update_set_arg_no_required_tables>{};
+      }
+    else
+      return succeeded{};
+  }
+
+  struct no_update_set_t
+  {
+  };
+
+  template <>
+  constexpr auto clause_tag<no_update_set_t> = clause::update_set{};
+
+  template <typename Statement>
+  class clause_base<no_update_set_t, Statement>
+  {
+  public:
+    template <typename OtherStatement>
+    constexpr clause_base(const clause_base<no_update_set_t, OtherStatement>& s)
+    {
+    }
+
+    constexpr clause_base() = default;
+
+    template <typename Table>
+    [[nodiscard]] constexpr auto update_set(Table t) const
+    {
+      constexpr auto check = check_update_set_arg(t);
+      if
+        constexpr(check)
+        {
+          return Statement::of(this).template replace_clause<no_update_set_t>(update_set_t<Table>{t});
+        }
+      else
+      {
+        return ::sqlpp::bad_statement_t<std::decay_t<decltype(check)>>{};
+      }
+    }
+  };
+
+  template <typename Context, typename Statement>
+  decltype(auto) operator<<(Context& context, const clause_base<no_update_set_t, Statement>&)
+  {
+    return context;
+  }
+
+  template <typename Table>
+  [[nodiscard]] constexpr auto update_set(Table&& t)
+  {
+    return statement<no_update_set_t>{}.update_set(std::forward<Table>(t));
+  }
+}
+
